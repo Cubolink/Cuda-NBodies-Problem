faf8e3fc64465024637e7d0c2c657e5529c1a20d
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2023-06-07 22:22:57 -0400

    feat: exports csv data, from a store in the timer

--- source/nbodies-util/particle-timer.cpp
+++ source/nbodies-util/particle-timer.cpp
@@ -3,8 +3,11 @@
 // ==================================
 
 #include "particle-timer.h"
+#include <string>
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <filesystem>
 
 ParticleTimer::ParticleTimer(int nParticles)
 : m_numParticles(nParticles),
@@ -24,6 +27,10 @@ void ParticleTimer::endIteration()
     double elapsedTime = std::chrono::duration<double>(m_endTime - m_startTime).count();
     m_totalElapsedTime += elapsedTime;
     m_iterationCount++;
+
+    double averageElapsedTime = m_totalElapsedTime / m_iterationCount;
+    store["iterations"].push_back(std::make_pair(m_totalElapsedTime, m_iterationCount));
+    store["particles_per_second"].push_back(std::make_pair(m_totalElapsedTime, m_numParticles / averageElapsedTime));
 }
 
 void ParticleTimer::printParticleEvaluatedPerSecond()
@@ -31,4 +38,31 @@ void ParticleTimer::printParticleEvaluatedPerSecond()
     double averageElapsedTime = m_totalElapsedTime / m_iterationCount;
     double particlesPerSecond = m_numParticles / averageElapsedTime;
     std::cout << "Particles evaluated per second: " << particlesPerSecond << std::endl;
+}
+
+void ParticleTimer::exportData(const std::string &folderPath) {
+    std::cout << "Exporting "<< std::endl;
+
+    std::filesystem::create_directories(folderPath);
+
+    // For each vector of the store
+    for (auto it = store.begin(); it != store.end(); ++it)
+    {
+        std::string key = it->first;
+        std::vector<std::pair<double, double>> values = it->second;
+
+        // Write into a representative csv file all the pairs from the vector
+        std::string filePath = folderPath + key + ".csv";
+        std::ofstream file(filePath);
+        for (auto vIt = values.begin(); vIt != values.end(); ++vIt)
+        {
+            double p1 = vIt->first;
+            double p2 = vIt->second;
+            if (file.is_open()) {
+                file << p1 << "," << p2 << std::endl;
+            } else
+                std::cout << "Failed to open the file: " << filePath << std::endl;
+        }
+        file.close();
+    }
 }
